f3fe948c72b06286e2cf12a0430113e06a572ced
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2021-12-14 14:38:13 -0600

    Update macos settings

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 echo "Setting up your Mac..."
 
+# Hide "last login" line when starting a new terminal session
+touch $HOME/.hushlogin
+
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
